8f170af09bfdbaa9ac2ac5d29887cc11d016df2e
Vihren Ganev <vcrazy@abv.bg>
2014-11-08 08:51:43 +0200

    request all static files

--- server.js
+++ server.js
@@ -7,25 +7,32 @@ server.listen(port);
 console.log('Listening on port ' + port);
 
 // GET /index.html
-app.get('/', function (req, res) {
-	res.sendFile(__dirname + '/webroot/index.html');
+app.get('/*', function (req, res) {
+	var requested_file = req.url;
+
+	if (requested_file === '/') {
+		requested_file += 'index.html';
+	}
+
+	console.log('Server static content ' + requested_file);
+	res.sendFile(__dirname + '/webroot' + requested_file);
 });
 
 // Start app code
 io.on('connection', function (socket) {
 
 	// generate random side
-	var sizes= ['left','right','top','down'];
+	var sizes = ['left', 'right', 'top', 'down'];
 	// shout for that side
-	setInterval(function(){
-    	socket.emit('sizes', { size: sizes[Math.floor(Math.random()*4)] });
-	}, 5000);
+	setInterval(function () {
+		socket.emit('sizes', {size: sizes[Math.floor(Math.random() * 4)]});
+	}, 1000);
 
 	// join a room
-	socket.emit('subscribe',1);
-	socket.on('subscribe', function(data) { 
+	socket.emit('subscribe', 1);
+	socket.on('subscribe', function (data) {
 		socket.join(data.room);
-		socket.emit('You are in room '+data.room);
+		socket.emit('You are in room ' + data.room);
 	});
 
 
