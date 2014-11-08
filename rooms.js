9fb34c4e29d65b50cb139820b851987e5ff02b02
Vihren Ganev <vcrazy@abv.bg>
2014-11-08 11:14:38 +0200

    joining now works well

--- rooms.js
+++ rooms.js
@@ -1,27 +1,39 @@
-function Rooms (data) {
-	this.roomid=0 | data.roomid;
-    this.name = data.name;
-    this.total_players = data.total_players;
-    this.gridx = data.gridx;
-    this.gridy = data.gridy;
-    this.number_of_players=data.number_of_players;
+function Rooms(data) {
+	this.name = data.name;
+	this.total_players = data.total_players;
+	this.gridx = data.gridx;
+	this.gridy = data.gridy;
+	this.number_of_players = data.number_of_players;
+	this.players_list = [];
 
-    // get the name of the room
-    this.getRoomName = function() {
-        return this.name;
-    };
-    this.setOneMorePlayer = function() {
-        this.number_of_players++;
-    };
-    this.removeOnePlayer = function() {
-        this.number_of_players--;
-    };
-    this.isFull = function() {
-        if (this.total_players===this.number_of_players)
-        	return true;
-        
-        return false;
-    };
+	// get the name of the room
+	this.getRoomName = function () {
+		return this.name;
+	};
+	this.setOneMorePlayer = function (player) {
+		this.number_of_players++;
+		this.players_list.push(player);
+	};
+	this.removeOnePlayer = function (player) {
+		this.number_of_players--;
+		this.players_list.splice(this.players_list.indexOf(player), 1);
+	};
+	this.isFull = function () {
+		if (this.total_players === this.number_of_players)
+			return true;
+
+		return false;
+	};
+	this.already_joined = function (player) {
+		return this.players_list.indexOf(player) > -1;
+	};
+	this.get_statistics = function () {
+		return {
+			name: this.name,
+			current_players: this.number_of_players,
+			max_players: this.total_players
+		};
+	};
 }
 
-module.exports = {'rooms':Rooms};
+module.exports = {Rooms: Rooms};
--- server.js
+++ server.js
@@ -2,18 +2,18 @@ var app = require('express')(),
 	server = require('http').Server(app),
 	io = require('socket.io')(server),
 	port = 3000,
-	rooms = require('./rooms.js'),
+	Rooms = require('./rooms.js'),
 	rooms_list = new Array();
 
 server.listen(port);
 console.log('Listening on port ' + port);
 
 // For testing purposes
-var newRoom1 = new rooms.rooms({name: 'Noobs', total_players: 5, gridx: 1000, gridy: 1000, number_of_players: 0, roomid: 1});
+var newRoom1 = new Rooms.Rooms({name: 'Noobs', total_players: 5, gridx: 1000, gridy: 1000, number_of_players: 0});
 rooms_list.push(newRoom1);
-var newRoom2 = new rooms.rooms({name: 'Mellee', total_players: 5, gridx: 1200, gridy: 600, number_of_players: 0, roomid: 2});
+var newRoom2 = new Rooms.Rooms({name: 'Mellee', total_players: 5, gridx: 1200, gridy: 600, number_of_players: 0});
 rooms_list.push(newRoom2);
-var newRoom3 = new rooms.rooms({name: 'Deathmatch', total_players: 5, gridx: 1200, gridy: 800, number_of_players: 0, roomid: 3});
+var newRoom3 = new Rooms.Rooms({name: 'Deathmatch', total_players: 5, gridx: 1200, gridy: 800, number_of_players: 0});
 rooms_list.push(newRoom3);
 
 // GET static content
@@ -62,28 +62,49 @@ io.on('connection', function (socket) {
 	socket.on('joinRoom', function (data) {
 		console.log('before', rooms_list);
 		// check for existing room
-		var i = 0;
-		var room_found = 0;
-		while (i < rooms_list.length) {
-			if (rooms_list[i].getRoomName() === data.room) {
-				room_found = 1;
-				break;
-			}
-			i++;
-		}
+		var room_name = data.room,
+			room = rooms_list.filter(function (room) {
+				return room.getRoomName() === room_name;
+			});
 
-		// if room exists
-		if (room_found === 1) {
-			// if room is full
-			if (rooms_list[i].isFull()) {
-				socket.emit('joinRoom', data.room + ' is full');
+//		var i = 0;
+//		var room_found = 0;
+//		while (i < rooms_list.length) {
+//			if (rooms_list[i].getRoomName() === data.room) {
+//				room_found = 1;
+//				break;
+//			}
+//			i++;
+//		}
+//
+//		// if room exists
+//		if (room_found === 1) {
+//			// if room is full
+//			if (rooms_list[i].isFull()) {
+//				socket.emit('joinRoom', data.room + ' is full');
+//			} else {
+//				socket.join(data.room);
+//				rooms_list[i].setOneMorePlayer();
+//				socket.emit('joinRoom', 'You are in room ' + data.room);
+//				socket.emit('rooms_statistics', rooms_list);
+//			}
+//		} else {
+//			socket.emit('joinRoom', 'Room with that name does not exists: ' + data.room);
+//		}
+
+		if (room[0]) {
+			if (room[0].isFull()) {
+				socket.emit('joinRoom', room_name + ' is full');
+			} else if (room[0].already_joined(socket.id)) {
+				socket.emit('joinRoom', 'You already play in ' + room_name);
 			} else {
 				socket.join(data.room);
-				rooms_list[i].setOneMorePlayer();
+				room[0].setOneMorePlayer(socket.id);
 				socket.emit('joinRoom', 'You are in room ' + data.room);
+				socket.emit('rooms_statistics', rooms_list);
 			}
 		} else {
-			socket.emit('joinRoom', 'Room with that name does not exists: ' + data.room);
+			socket.emit('joinRoom', 'Room with that name does not exists: ' + room_name);
 		}
 
 		console.log('after', rooms_list);
@@ -106,7 +127,18 @@ io.on('connection', function (socket) {
 			socket.leave(data.room);
 			rooms_list[i].removeOnePlayer();
 			socket.emit('leaveroom', 'You just left room ' + data.room);
+			emit_rooms_statistics();
 		}
 	});
+
+	socket.on('get_rooms', function () {
+		emit_rooms_statistics();
+	});
+
+	function emit_rooms_statistics(){
+		socket.emit('rooms_statistics', rooms_list.map(function(room){
+			return room.get_statistics();
+		}));
+	}
 
 });
