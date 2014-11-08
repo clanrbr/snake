79f924ee686b6447f1edae32b137f0a95f17c4b7
clanrbr <clan_rbr@yahoo.com>
2014-11-08 19:29:19 +0200

    added info for rooms

--- webroot/js.js
+++ webroot/js.js
@@ -18,9 +18,22 @@ window.onload = function () {
 	socket.on('rooms_statistics', function (rooms) {
 		// rooms.filter
 		room1=document.getElementById('room1').innerHTML=rooms[0].name;
-		roominfo=document.getElementById
+		document.getElementById('roomname1').innerHTML=rooms[0].name;
+		document.getElementById('roomdescription1').innerHTML=rooms[0].description;
+		document.getElementById('current_players1').innerHTML=rooms[0].current_players;
+		document.getElementById('max_players1').innerHTML=rooms[0].max_players;
+
 		room2=document.getElementById('room2').innerHTML=rooms[1].name;
+		document.getElementById('roomname2').innerHTML=rooms[1].name;
+		document.getElementById('roomdescription2').innerHTML=rooms[1].description;
+		document.getElementById('current_players2').innerHTML=rooms[1].current_players;
+		document.getElementById('max_players2').innerHTML=rooms[1].max_players;
+
 		room3=document.getElementById('room3').innerHTML=rooms[2].name;
+		document.getElementById('roomname3').innerHTML=rooms[2].name;
+		document.getElementById('roomdescription3').innerHTML=rooms[2].description;
+		document.getElementById('current_players3').innerHTML=rooms[2].current_players;
+		document.getElementById('max_players3').innerHTML=rooms[2].max_players;
 
 		console.log('rooms_statistics', rooms);
 	});
